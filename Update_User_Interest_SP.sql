9f3639d8f07266e146b674fc9c7cbb41d4c0b99c
Balaji Ganesh 3028 <balaji.ganesan@kanini.com>
2025-06-21 18:00:06 +0530

    Fix typo in type name for user interest updates; correct INTREST_Update_LIST to INTEREST_Update_LIST

--- Update_User_Interest_SP.sql
+++ Update_User_Interest_SP.sql
@@ -1,5 +1,5 @@
 CREATE OR ALTER PROCEDURE UpdateUserInterest
-    @UpdateUserInterests INTREST_Update_LIST READONLY
+    @UpdateUserInterests INTEREST_Update_LIST READONLY
 AS
 BEGIN
     UPDATE UI
@@ -12,14 +12,14 @@ BEGIN
         ON UI.UserId = UUI.UserId AND UI.Id = UUI.Interest_Id;
 END;
 
-CREATE TYPE INTREST_Update_LIST AS Table(
-	UserId INT NULL,
-	Interest_Id INT NULL,
-	Interest_Value VARCHAR(200) NULL
+CREATE TYPE INTEREST_Update_LIST AS Table(
+    UserId INT NULL,
+    Interest_Id INT NULL,
+    Interest_Value VARCHAR(200) NULL
 )
 
 -- STEP 1: Declare variable of the TVP type
-DECLARE @UserInterestUpdates AS INTREST_Update_LIST;
+DECLARE @UserInterestUpdates AS INTEREST_Update_LIST;
 
 -- STEP 2: Insert test data into the variable using separate statements
 INSERT INTO @UserInterestUpdates (UserId, Interest_Id, Interest_Value)
